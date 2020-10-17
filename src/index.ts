96bc0e2d51a9cb0124bd11be9a22b99297a3b078
freddneos <fredd@neosdev.com.br>
2020-10-17 13:52:22 +0100

    chore: tslint cfg

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,6 @@ import * as functions from 'firebase-functions';
 
  export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
+
+  
  });
