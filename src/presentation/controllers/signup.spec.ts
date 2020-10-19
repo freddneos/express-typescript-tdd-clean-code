6aa09a2d2b12e0063a76ff94f80b1f0a33b55093
freddneos <fredd@neosdev.com.br>
2020-10-19 20:21:30 +0100

    test: signupController  return 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -5,7 +5,6 @@ describe('Signup Controller ', () => {
         const sut = new SignupController();
         const httpRequest = {
             body: {
-                name: 'any_name',
                 email: 'any_email',
                 password: 'any_password',
                 passwordConfirmation: 'any_password'
@@ -15,6 +14,23 @@ describe('Signup Controller ', () => {
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+
+    })
+})
+
+describe('Signup Controller ', () => {
+    test('should return 400 if no email is provided', () => {
+        const sut = new SignupController();
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error('Missing param: email'));
 
     })
 })
