293f50310e84074739e8890ae0a966bcaf74d690
freddneos <fredd@neosdev.com.br>
2020-10-19 22:00:13 +0100

    feat: email validator implementation

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,8 +2,15 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error copy'
 
 export class SignupController implements Controller {
+    private readonly emailValidator: EmailValidator
+
+    constructor(emailValidator: EmailValidator) {
+        this.emailValidator = emailValidator
+    }
     handle(httpRequest: HttpRequest): HttpResponse {
         const requiredFields = ['email', 'name', 'password', 'passwordConfirmation']
         for (const field of requiredFields) {
@@ -11,9 +18,9 @@ export class SignupController implements Controller {
                 return badRequest(new MissingParamError(field))
             }
         }
-        return {
-            statusCode: 200,
-            body: ''
+        const isValid = this.emailValidator.isValid(httpRequest.body.email)
+        if (!isValid) {
+            return badRequest(new InvalidParamError('email'))
         }
     }
 }
