8a479245f63c28f9193477a0340a2903984a0b98
freddneos <fredd@neosdev.com.br>
2020-10-19 21:25:07 +0100

    refactor: controller interface to implement

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,14 +1,19 @@
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
+import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
-export class SignupController {
+export class SignupController implements Controller {
     handle(httpRequest: HttpRequest): HttpResponse {
-        const requiredFields = ['email', 'name']
+        const requiredFields = ['email', 'name', 'password', 'passwordConfirmation']
         for (const field of requiredFields) {
             if (!httpRequest.body[field]) {
                 return badRequest(new MissingParamError(field))
             }
+        }
+        return {
+            statusCode: 200,
+            body: ''
         }
     }
 }
