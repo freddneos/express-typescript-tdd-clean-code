6be51d93b917251965576ce19ff5f4eb9e44d89c
freddneos <fredd@neosdev.com.br>
2020-10-21 11:08:14 +0100

    chore: adjust jest configuration to ignore protocols and import and export only files

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
   coverageDirectory: "coverage",
   testEnvironment: "node",
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
 };
-  
