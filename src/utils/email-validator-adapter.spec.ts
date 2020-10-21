714a2a1b31427e58b0574942c25e2572e4509364
freddneos <fredd@neosdev.com.br>
2020-10-21 22:35:53 +0100

    refactor:  a EmailValidator to factory method

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -2,12 +2,14 @@ import validator from 'validator'
 import { EmailValidatorAdapter } from './email-validator'
 
 
-
+const makeSut = (): EmailValidatorAdapter => {
+    return new EmailValidatorAdapter()
+}
 
 describe('EmailValidatorAdapter', () => {
     test('Should return false if validator returns false', () => {
 
-        const sut = new EmailValidatorAdapter()
+        const sut = makeSut()
         jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
         const isValid = sut.isValid('invalid_email@mail.com')
         expect(isValid).toBe(false)
@@ -15,14 +17,14 @@ describe('EmailValidatorAdapter', () => {
 
     test('Should return true if validator returns true', () => {
 
-        const sut = new EmailValidatorAdapter()
+        const sut = makeSut()
         const isValid = sut.isValid('valid_email@mail.com')
         expect(isValid).toBe(true)
     })
 
     test('Should call validator with correct email', () => {
 
-        const sut = new EmailValidatorAdapter()
+        const sut = makeSut()
         const isEmailSpy = jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
         sut.isValid('any_email@mail.com')
         expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
